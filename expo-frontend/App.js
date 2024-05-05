ecd92faf403f2850fd5d2371fee6c67ab3230935
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-05-05 00:24:49 -0400

    merged properly

--- expo-frontend/App.js
+++ expo-frontend/App.js
@@ -1,50 +1,10 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
 
 // Import Screens
 import Login from './screens/Login';
 import Signup from './screens/Signup';
 import Welcome from './screens/Welcome';
-=======
 import React, { useState, useEffect, useCallback } from 'react';
 import { View } from 'react-native';
->>>>>>> eric-branch
-
-// Import RootStack
-import RootStack from './navigators/RootStack';
-import { useLocalNotification } from './notifications/useLocalNotification';
-import * as Notifications from "expo-notifications";
-import { schedulePushNotification } from "./notifications/handleNotifications";
-import { Button } from "react-native";
-import ReminderPickerButton from "./notifications/ReminderPickerButton";
-
-<<<<<<< HEAD
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false
-  })
-});
-export default function App() {
-  
-  useLocalNotification();
-  const [reminderInterval, setReminderInterval] = useState(8);
-  
-  //console.log("intervalSEC BEFORE: ",reminderInterval)
-  const handleLocalPushNotification = async (reminderInterval) => {
-    let intervalInSeconds = reminderInterval * 3600;
-    //console.log("intervalSEC ",intervalInSeconds);
-    await schedulePushNotification(intervalInSeconds);
-  };
-  return (
-    <>
-      <RootStack
-        reminderInterval={reminderInterval}
-        setReminderInterval={setReminderInterval}
-        handleLocalPushNotification={handleLocalPushNotification}
-      />
-=======
 // Async storage
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -53,10 +13,23 @@ import { CredentialsContext } from './components/CredentialsContext';
 
 // Splash Screen
 import * as SplashScreen from 'expo-splash-screen';
+// Import RootStack
+import RootStack from './navigators/RootStack';
+import { useLocalNotification } from './notifications/useLocalNotification';
+import * as Notifications from "expo-notifications";
+import { schedulePushNotification } from "./notifications/handleNotifications";
+import { Button } from "react-native";
+import ReminderPickerButton from "./notifications/ReminderPickerButton";
 
-// Keep the splash screen visible while we fetch resources
 //SplashScreen.preventAutoHideAsync();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+});
 export default function App() {
   const [appReady, setAppReady] = useState(false);
   const [storedCredentials, setStoredCredentials] = useState("");
@@ -81,16 +54,23 @@ export default function App() {
     checkLoginCredentials();
   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    SplashScreen.hideAsync();
-  }, [appReady]);
-
+  useLocalNotification();
+  const [reminderInterval, setReminderInterval] = useState(8);
+  
+  //console.log("intervalSEC BEFORE: ",reminderInterval)
+  const handleLocalPushNotification = async (reminderInterval) => {
+    let intervalInSeconds = reminderInterval * 3600;
+    //console.log("intervalSEC ",intervalInSeconds);
+    await schedulePushNotification(intervalInSeconds);
+  };
   return (
-    <>
-      <CredentialsContext.Provider value={{storedCredentials, setStoredCredentials}}>
-        <RootStack/>
-      </CredentialsContext.Provider>
->>>>>>> eric-branch
-    </>
+    <CredentialsContext.Provider value={{storedCredentials, setStoredCredentials}}>
+
+      <RootStack
+        reminderInterval={reminderInterval}
+        setReminderInterval={setReminderInterval}
+        handleLocalPushNotification={handleLocalPushNotification}
+      />
+     </CredentialsContext.Provider>
   );
 } 
